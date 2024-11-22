226ea6de9670b4cb03e515cc85d6b639a5c754f5
Bielszanetti <gabrielcgbr2018@gmail.com>
2024-11-22 20:03:41 -0300

    ultima aula feita
    cors
    front
    put
    testes

--- Projeto/src/controllers/postsController.js
+++ Projeto/src/controllers/postsController.js
@@ -1,7 +1,58 @@
-import getTodosPosts from "../model/postsModel.js";
+import {getTodosPosts, criarPost, autlizarPost} from "../model/postsModel.js";
+import fs from "fs"
+import gerarDescricaoComGemini from "../service/geminiService.js";
 
 export async function listarPosts (req, res) {
     const posts = await getTodosPosts();
     res.status(200).json(posts);
 }
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try{
+        const PostCriado = await criarPost(novoPost);
+        res.status(200).json(PostCriado);
+    } catch(erro) {
+        console.error(erro.menssage);
+        res.status(500).json({"Erro": "Falha na Requisação"})
+    }
+}
+
+export async function upLoadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    }
+    try{
+        const PostCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${PostCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(PostCriado);
+    } catch(erro) {
+        console.error(erro.menssage);
+        res.status(500).json({"Erro": "Falha na Requisação"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
     
+    try{
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricaoIA = await gerarDescricaoComGemini(imgBuffer);
+
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricaoIA,
+            alt: req.body.alt
+        }
+
+        const PostCriado = await autlizarPost(id, post);
+        res.status(200).json(PostCriado);
+    } catch(erro) {
+        console.error(erro.menssage);
+        res.status(500).json({"Erro": "Falha na Requisação"})
+    }
+}
--- Projeto/src/model/postsModel.js
+++ Projeto/src/model/postsModel.js
@@ -1,10 +1,26 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"; 
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // função para buscar Banco e a Coleção
-export default async function getTodosPosts() {
-    const db = conexao.db("Imersão-Backend");
+export async function getTodosPosts() {
+    const db = conexao.db("Imerção-Backend");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
 }
+
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("Imerção-Backend");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
+}
+
+export async function autlizarPost(id, novoPost) {
+    const db = conexao.db("Imerção-Backend");
+    const colecao = db.collection("posts");
+    const objId = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objId)}, {$set:novoPost});
+}
+
